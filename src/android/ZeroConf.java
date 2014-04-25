dbf39924d699be41de387a1aba9c154d40b92501
Soren Juul Rasmussen <sorenjuul@gmail.com>
2014-04-25 09:42:02 +0200

    error hanling

--- src/android/ZeroConf.java
+++ src/android/ZeroConf.java
@@ -117,14 +117,13 @@ public class ZeroConf extends CordovaPlugin {
 
 		} else if (action.equals("close")) {
 			if (jmdns != null) {
-                    try {
-                        jmdns.close(); // Thread-safe.
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        return;
-                    }
+                try {
+                    jmdns.close(); // Thread-safe.
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return;
                 }
-			}
+            }
             return true;
 		} else if (action.equals("unregister")) {
 			if (jmdns != null) {
